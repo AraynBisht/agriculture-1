7b0f0eec9989acc15f07f60e02113a0e0297a672
AraynBisht <aryansingh.bisht2022@vitstudent.ac.in>
2024-10-07 11:15:52 +0530

    Delete public directory

--- public/js/conifg.js
+++
@@ -1,8 +0,0 @@
-module.exports.awsConfig = {
-    awsConfig: {
-      accessKeyId: 'AKIARHJJMYASPIIKIVK4',
-      secretAccessKey: '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',
-      region: 'ap-south-1',
-      bucketName:'argi-03'
-    }
-  }
